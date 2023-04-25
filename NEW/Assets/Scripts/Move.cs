1703591eacc501cd3fff8396ccf57ce60bc6372f
Lucien <jean20011116@gmail.com>
2023-04-25 17:38:39 +0800

    0425

--- NEW/Assets/Scripts/Move.cs
+++ NEW/Assets/Scripts/Move.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Move : MonoBehaviour
 {
     public Vector3 goal = new Vector3(3, 0, 5);
+    public float speed = 0.1f;
 
     void Start()
     {
-        goal = goal * 0.01f;
+        goal = goal * 0.1f;
     }
 
     void Update()
     {
-        this.transform.Translate(goal);
+        this.transform.Translate(goal.normalized * speed);
     }
 }
