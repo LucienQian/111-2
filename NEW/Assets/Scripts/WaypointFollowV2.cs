2b972315600dde86b26691b00e8bffbc35de8750
Lucien <jean20011116@gmail.com>
2023-05-09 17:39:49 +0800

    0509

--- NEW/Assets/Scripts/WaypointFollowV2.cs
+++ NEW/Assets/Scripts/WaypointFollowV2.cs
@@ -2,47 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaypointFollow : MonoBehaviour
+public class WaypointFollowV2 : MonoBehaviour
 {
-    public GameObject[] waypoints;
+    //public GameObject[] waypoints;
+    public UnityStandardAssets.Utility.WaypointCircuit circuit;
     public int currentWP = 0;
 
-    public Transform goal;
+    //public Transform goal;
     public float speed = 1;
     public float closePoint = 0.5f;
     public float rotSpeed = 5;
-    
+
 
     void Start()
     {
-        waypoints = GameObject.FindGameObjectsWithTag("tag-waypoint");
+        //waypoints = GameObject.FindGameObjectsWithTag("tag-waypoint");
     }
 
     void Update()
     {
-        if (waypoints.Length == 0) return;
+        if (circuit.Waypoints.Length == 0) return;
 
-        Vector3 lookAtGoal = new Vector3(waypoints[currentWP].transform.position.x,
+        Vector3 lookAtGoal = new Vector3(circuit.Waypoints[currentWP].transform.position.x,
                                         this.transform.position.y,
-                                        waypoints[currentWP].transform.position.z);
+                                        circuit.Waypoints[currentWP].transform.position.z);
 
         Vector3 direction = lookAtGoal - this.transform.position;
-        Vector3 directionVertical = lookAtGoal - waypoints[currentWP].transform.position; 
+        Vector3 directionVertical = lookAtGoal - circuit.Waypoints[currentWP].transform.position;
         Debug.DrawRay(this.transform.position, direction, Color.green);
-        Debug.DrawRay(goal.transform.position, directionVertical, Color.red); waypoints[currentWP]
+        Debug.DrawRay(circuit.Waypoints[currentWP].transform.position, directionVertical, Color.red);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
                                                 Quaternion.LookRotation(direction),
                                                 Time.deltaTime * rotSpeed);
 
         //if (Vector3.Distance(this.transform.position, lookAtGoal) > closePoint)
         //{
-            //this.transform.Translate(0, 0, speed * Time.deltaTime);
+        //this.transform.Translate(0, 0, speed * Time.deltaTime);
         //}
 
-        if(direction.magnitude < closePoint)
+        if (direction.magnitude < closePoint)
         {
             currentWP++;
-            if(currentWP >= waypoints.Length)
+            if (currentWP >= circuit.Waypoints.Length)
             {
                 currentWP = 0;
             }
