3aec86903d32651734e5ffb2715211daf06f72ac
Lucien <jean20011116@gmail.com>
2023-05-09 15:44:42 +0800

    0509

--- NEW/Assets/Scripts/MoveLocal.cs
+++ NEW/Assets/Scripts/MoveLocal.cs
@@ -2,36 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLocal : MonoBehaviour
-{
-    public float speed = 1.0f;
-    public Transform goal;
-    public float closePoint = 1f;
-    public float rotSpeed = 0.5f;
-    void Start()
-    {
-        
-    }
+public class MoveLocal : MonoBehaviour {
 
-    void Update()
-    {
-        //新向量(水平)
-        Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+	public Transform goal;
+	float speed = 0.5f;
+	float accuracy = 1.0f;
 
-        //this.transform.LookAt(goal.position);
-
-        //舊向量(可傾斜)
-        Vector3 direction = goal.position - this.transform.position;//目標物的向量
-        Debug.DrawRay(this.transform.position, direction, Color.blue);
-
-        // this.transform.LookAt(lookAtGoal);
-
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
-
-        if(Vector3.Distance(this.transform.position, lookAtGoal) > closePoint)
-        {
-            this.transform.Translate(direction.normalized * speed * Time.deltaTime);//殭屍移動
-        }
-
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 lookAtGoal = new Vector3(goal.position.x, 
+										this.transform.position.y, 
+										goal.position.z);
+		this.transform.LookAt(lookAtGoal);
+		if(Vector3.Distance(transform.position,lookAtGoal) > accuracy)
+			this.transform.Translate(0,0,speed*Time.deltaTime);
+	}
 }
---
+++ NEW/Assets/Scripts/WaypointFollow.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollow : MonoBehaviour
+{
+    public Transform goal;
+    public float speed = 1;
+    public float closePoint = 0.5f;
+    public float rotSpeed = 5;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 lookAtGoal = new Vector3(goal.position.x,
+                                        this.transform.position.y,
+                                        goal.position.z);
+
+        Vector3 direction = lookAtGoal - this.transform.position;
+        Vector3 directionVertical = lookAtGoal - goal.transform.position; 
+        Debug.DrawRay(this.transform.position, direction, Color.green);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                Quaternion.LookRotation(direction),
+                                                Time.deltaTime * rotSpeed);
+
+        if (Vector3.Distance(this.transform.position, lookAtGoal) > closePoint)
+        {
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+
+
+    }
+}
